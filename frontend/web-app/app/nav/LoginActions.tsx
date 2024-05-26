3706f5637f3f5a6dd7feeb2c98c5b13d474005ec
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-05-26 16:47:01 +1000

    Revert "update loginbutton"
    This reverts commit be4af2824cabdadd9b17eed1c4fa971ce187879d.

--- frontend/web-app/app/nav/LoginActions.tsx
+++ frontend/web-app/app/nav/LoginActions.tsx
@@ -8,8 +8,8 @@ import { RiLoginBoxLine } from "react-icons/ri";
 const LoginActions = () => {
   return (   
         <Dropdown arrowIcon={false} label={<FaRegCircleUser className="m-0 mr-1 text-green-600 hover:text-green-400 p-0" size={30} />} inline>
-                <Dropdown.Item className=' ' icon={RiLoginBoxLine} onClick={() => signIn("id-server",{callbackUrl:"/"}, {prompt: "login"})}>Login</Dropdown.Item>
-                {/* <Dropdown.Item className=' ' icon={FaGithub} onClick={()=>{signIn("github");}}>Sign in with Github</Dropdown.Item> */}
+                <Dropdown.Item className=' ' icon={RiLoginBoxLine} onClick={() => signIn("id-server",{callbackUrl:"https://carsbidi.onrender.com/"}, {prompt: "login"})}>Login</Dropdown.Item>
+                <Dropdown.Item className=' ' icon={FaGithub} onClick={()=>{signIn("github");}}>Sign in with Github</Dropdown.Item>
         </Dropdown>    
   )
 }
