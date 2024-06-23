b598849203cabc6d1a0886091df362e626e5424c
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-06-23 20:03:46 +1000

    Revert "Update fetchWrapper.ts"
    This reverts commit 2a94d4164d0c5997c5a36bef21516b220a91f3e8.

--- frontend/web-app/lib/fetchWrapper.ts
+++ frontend/web-app/lib/fetchWrapper.ts
@@ -124,11 +124,7 @@ async function del(url: string, retries = 5, backoff = 3000) {
 
 async function getHeaders() {
     const token = await getTokenWorkAround();
-    const headers = {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "User-Agent": "PostmanRuntime/7.28.4",
-    } as any;
+    const headers = { "Content-Type": "application/json" } as any;
     if (token) {
         headers.Authorization = `Bearer ${token.access_token}`;
     }
