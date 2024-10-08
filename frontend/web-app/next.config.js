c8d087d1ccb56dc6239eb3cbfcc4cec33add351e
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-10-09 02:30:14 +1100

    Update next.config.js

--- frontend/web-app/next.config.js
+++ frontend/web-app/next.config.js
@@ -7,11 +7,38 @@ const nextConfig = {
     images: {
         domains: ["cdn.pixabay.com", "media.istockphoto.com", "carma.com.au", "carsales.pxcrush.net"],
     },
+
     env: {
         NEXTAUTH_SECRET: process.env.CLIENT_SECRET, // "secret"
         NEXTAUTH_URL: process.env.CLIENT_APP, // "http://localhost:3000"
     },
     reactStrictMode: false,
+    async redirects() {
+        return [
+            {
+                source: "/(.*)",
+                has: [
+                    {
+                        type: "host",
+                        value: "^(?!localhost).*", // Matches all hosts except localhost
+                    },
+                ],
+                destination: "https://carsbidi.onrender.com/:path*", // Redirect to HTTPS in production
+                permanent: true,
+            },
+            {
+                source: "/(.*)",
+                has: [
+                    {
+                        type: "host",
+                        value: "localhost", // Matches localhost for development
+                    },
+                ],
+                destination: "https://localhost:3000/:path*", // Redirect to HTTPS for localhost
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
