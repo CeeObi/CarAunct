8a4e87939354751087baf48ccb3bcb5b3647dcb9
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-10-26 13:31:24 +1100

    Update next.config.js

--- frontend/web-app/next.config.js
+++ frontend/web-app/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     //     experimental:{
     //         serverActions:true,
     //     },
-    output: "export",
+    output: "standalone",
     images: {
         domains: ["cdn.pixabay.com", "media.istockphoto.com", "carma.com.au", "carsales.pxcrush.net"],
     },
