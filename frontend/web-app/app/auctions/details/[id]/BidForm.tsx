a9b9b639ab1481aa947d0ded1f6d1ecd8c65c0a2
ceeobi <chukwudimaobi@gmail.com>
2024-12-14 22:57:12 +1100

    general update

--- frontend/web-app/app/auctions/details/[id]/BidForm.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { placeBidsForAuctions } from "@/app/Services/auctionService";
+import { Button } from "flowbite-react";
 import useBidStore from "@/app/hooks/useBidStore";
 import { numberWithCommas } from "@/lib/numberWithComma";
 import React from "react";
@@ -24,6 +25,7 @@ function BidForm({ auctionId, highBid }: Props) {
             reset();
             return toast.error("Bid must be at least $" + numberWithCommas(highBid + 1));
         }
+        console.log(data);
         placeBidsForAuctions(auctionId, +data.amount)
             .then((bid) => {
                 if (bid.error) throw bid.error;
@@ -34,13 +36,21 @@ function BidForm({ auctionId, highBid }: Props) {
     }
 
     return (
-        <form onSubmit={handleSubmit(handleOnSubmit)} className="flex items-center border-2 rounded-lg py-2">
+        <form onSubmit={handleSubmit(handleOnSubmit)} className="flex items-center border-2 rounded-lg py-2 pr-2">
             <input
                 type="number"
                 {...register("amount")}
                 className="input-custom text-sm text-gray-600"
                 placeholder={`Enter your bid (minimum bid is ${numberWithCommas(highBid + 1)})`}
+                min={0}
             />
+            <Button
+                className="bg-green-500 px-7 hover:bg-green-700 text-neutral-200 hover:scale-105 duration-500 border-0 "
+                type="submit"
+                color="primary"
+            >
+                Place Bid
+            </Button>
         </form>
     );
 }
