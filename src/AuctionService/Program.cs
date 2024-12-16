e0c796d0692275a719750464247a4a7dd86bec40
ceeobi <chukwudimaobi@gmail.com>
2024-12-16 16:14:49 +1100

    Revert "commence integrate github oauth"
    This reverts commit a209b397d5e73e7c8ff9d3084460d1c5e54adaa1.

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -47,27 +47,13 @@ builder.Services.AddMassTransit(x =>
                     // cfg.ConfigureEndpoints(context);
                 });
             });
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer("IdentityServer", options => 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => 
 {
     options.Authority = builder.Configuration["IdentityServiceUrl"];
     options.RequireHttpsMetadata = false;
     options.TokenValidationParameters.ValidateAudience = false;
     options.TokenValidationParameters.NameClaimType = "username";
-})
-// 
-// .AddJwtBearer("GitHub",options => 
-// {
-//     options.Authority = "https://token.actions.githubusercontent.com";
-//     options.RequireHttpsMetadata = false;
-//     options.TokenValidationParameters.ValidateAudience = true;
-//     options.TokenValidationParameters.NameClaimType = "username";
-//     options.TokenValidationParametersValidAudience = builder.Configuration["GitHub:ValidAudience"];
-//     options.TokenValidationParametersValidateIssuerSigningKey = true;
-//     options.TokenValidationParametersValidateLifetime = true;
-// })
-;
-// 
+});
 
 builder.Services.AddGrpc();
 
